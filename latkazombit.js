84906587ca02103389875b997ea3ba0dc9de038b
Eino Keskitalo <eino.keskitalo@gmail.com>
2015-09-10 11:39:28 +0300

    Use the new findMonologue function in LZ script as well.

--- latkazombit.js
+++ latkazombit.js
@@ -33,7 +33,7 @@ stage.get('#start_game')[0].on('tap click', function(event) {
 stage.get('#start')[0].on('tap click', function(event) {
 	event = event.targetNode;
 
-	setMonologue(panic.getAttr('id'), 'text');
+	setMonologue(findMonologue(panic, 'text'));
 	setTimeout(function() {
 		idle_1.hide();
 		panic.show();
