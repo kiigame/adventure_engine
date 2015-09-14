f82ec16f15b0289f01c20eea30171a3263430e01
Eino Keskitalo <eino.keskitalo@gmail.com>
2015-09-14 10:15:43 +0300

    Put consume tags to object.json & correct the code accordingly.
    I'm not quite sure why the images/objects.json division exists, but I assume it was useful when working on the KiiGameEditor project. I'd rather access gameplay attributes from the same object though, instead of looking them up with a key from separate data.

--- kiigame.js
+++ kiigame.js
@@ -828,7 +828,7 @@ stage.get('Image').on('dragend', function(event) {
 
 			// Items may be consumed when used
 			dragged_object = objects_json[dragged_item.getAttr('object_name')];
-			if (dragged_item.getAttr('consume') === true)
+			if (dragged_object.consume === true)
 				destroy = true;
 
             // The object is destroyed if it is the target of item's use
