73dade5e917916e124f777f55f0ffea60af13ff8
Eino Keskitalo <eino.keskitalo@gmail.com>
2019-09-24 23:26:55 +0300

    Add a class for building text part of sequence slides.
    Unused at the moment (but with test!)

---
+++ test/view/sequence/testTextBuilder.js
@@ -0,0 +1,31 @@
+var chai = require('chai');
+var assert = chai.assert;
+TextBuilder = require('../../../view/sequence/TextBuilder.js');
+
+describe('Test sequence TextBuilder', function(){
+    it('it should build expected JSON object with Text data', function(){
+        let textBuilder = new TextBuilder();
+
+        var expected =                         {
+            "attrs": {
+                "text": "Kaikki ei kuitenkaan ollut niin kuin piti..",
+                "fontFamily": "Chalkboard SE",
+                "fontSize": 26,
+                "fill": "white",
+                "shadowColor": "#bbbbbb",
+                "shadowBlur": 10,
+                "width": 981,
+                "align": "center",
+                "y": 321
+            },
+            "className": "Text"
+        };
+        var text = {
+            "text": {
+                "text": "Kaikki ei kuitenkaan ollut niin kuin piti.."
+            }
+        };
+        var result = textBuilder.build(text);
+        assert.deepEqual(expected, result);
+    });
+});
