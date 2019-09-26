ae1c3cf17eafb1864947ae2e929a2e3bdce21fc3
Eino Keskitalo <eino.keskitalo@gmail.com>
2019-09-26 19:50:50 +0300

    Add SequencesBuilder class for building all sequences from sequences_json.

---
+++ test/view/sequence/testSequencesBuilder.js
@@ -0,0 +1,33 @@
+var chai = require('chai');
+var sinon = require('sinon');
+var assert = chai.assert;
+SequencesBuilder = require('../../../view/sequence/SequencesBuilder.js');
+SequenceBuilder = require('../../../view/sequence/SequenceBuilder.js');
+var sequenceBuilderStub = sinon.createStubInstance(SequenceBuilder);
+sequenceBuilderStub.build.withArgs([], "intro").returns({ "data": "intro"});
+sequenceBuilderStub.build.withArgs([], "outro").returns({ "data": "outro"});
+
+describe('Test SequencesBuilder', function(){
+    it('build full sequence data with two sequences', function(){
+        let sequencesBuilder = new SequencesBuilder(sequenceBuilderStub);
+
+        var expected = [
+            {
+                "data": "intro",
+            },
+            {
+                "data": "outro"
+            }
+        ];
+        var sequences = {
+            "intro": {
+                slides: []
+            },
+            "outro": {
+                slides: []
+            }
+        };
+        var result = sequencesBuilder.build(sequences);
+        assert.deepEqual(expected, result);
+    });
+});
