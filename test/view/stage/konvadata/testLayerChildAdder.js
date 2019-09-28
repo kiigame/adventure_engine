1b0e11dfd31ff7a27a842aa739f28acbd632fd8b
Eino Keskitalo <eino.keskitalo@gmail.com>
2019-09-28 19:47:01 +0300

    Add a class for adding chidlren to a layer in the data for Konva.

---
+++ test/view/stage/konvadata/testLayerChildAdder.js
@@ -0,0 +1,49 @@
+var chai = require('chai');
+var assert = chai.assert;
+LayerChildAdder = require('../../../../view/stage/konvadata/LayerChildAdder.js');
+
+describe('Test stage LayerChildAdder', function(){
+    it('should add children to the specified layer', function(){
+        let layerChildAdder = new LayerChildAdder();
+
+        var expected = {
+            'children': [
+                {
+                    "attrs": {
+                        "id": 'first'
+                    },
+                    "children": [
+                        {
+                            "id": 'item1'
+                        },
+                        {
+                            "id": 'item2'
+                        }
+                    ]
+                }
+            ]
+        };
+        var imagesJson = {
+            'children': [
+                {
+                    "attrs": {
+                        "id": 'first'
+                    },
+                    "children": []
+                }
+            ]
+        };
+        var newJson = [
+            {
+                "id": 'item1'
+            },
+            {
+                "id": 'item2'
+            }
+        ];
+        var result = layerChildAdder.add(imagesJson, newJson, 'first');
+        assert.deepEqual(expected, result);
+    });
+
+    // TODO: Test case where layer is not found in imagesJson
+});
