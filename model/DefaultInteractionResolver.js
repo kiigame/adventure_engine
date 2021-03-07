e54b726cda57398d2eb5f5153233bc7cb97cb0ef
Eino Keskitalo <eino.keskitalo@gmail.com>
2021-03-07 23:46:56 +0200

    Untested refactor of click/drag resolution to commands.

---
+++ model/DefaultInteractionResolver.js
@@ -0,0 +1,33 @@
+import Interactions from "./Interactions.js";
+
+/** */
+class DefaultInteractionResolver {
+
+    constructor(targetCategory) {
+        this.targetCategory = targetCategory;
+    }
+
+    getTargetCategory() {
+        return this.targetCategory;
+    }
+
+    /**
+     * @param Interactions interactions
+     * @param string targetId
+     * @param string action (click or dragged item)
+     * @param string defaultString (examine or dragged item)
+     */
+    resolveCommands(interactions, targetId, action = 'click', defaultString = 'examine') {
+        var commands;
+        commands = interactions.getCommands(targetId, action);
+
+        // no interaction for the action defined: usual text
+        if (commands === null) {
+            commands = [{"command":"monologue", "textkey":{"object": targetId, "string": defaultString}}];
+        }
+
+        return commands;
+    }
+}
+
+export default DefaultInteractionResolver;
