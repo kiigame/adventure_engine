9c8ad76f23b1402eec9bdb7f7ff0580b21f4f6ee
Eino Keskitalo <eino.keskitalo@gmail.com>
2021-03-08 11:38:37 +0200

    Deploy some console logging

--- model/DefaultInteractionResolver.js
+++ model/DefaultInteractionResolver.js
@@ -18,8 +18,10 @@ class DefaultInteractionResolver {
      * @param string defaultString (examine or dragged item)
      */
     resolveCommands(interactions, targetId, action = 'click', defaultString = 'examine') {
+        console.log(targetId + " " + action + " " + defaultString);
         var commands;
         commands = interactions.getCommands(targetId, action);
+        console.log(commands);
 
         // no interaction for the action defined: usual text
         if (commands === null) {
