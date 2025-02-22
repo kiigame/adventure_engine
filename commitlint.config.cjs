2e54b6fb5be3b6c1ff46f681d13b233b19868723
Eino Keskitalo <eino.keskitalo@gmail.com>
2025-02-22 11:24:17 +0200

    build: enforce present-imperative tense in commit messages for clarity

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,5 +1,17 @@
 module.exports = {
-    extends: [
-        "@commitlint/config-conventional"
-    ],
+  extends: [
+    "@commitlint/config-conventional"
+  ],
+  plugins: ['commitlint-plugin-tense'],
+  rules: {
+    'tense/subject-tense': [
+      1,
+      'always',
+      {
+        allowedTenses: ['present-imperative'],
+        firstOnly: true,
+        allowlist: [],
+      }
+    ]
+  }
 }
