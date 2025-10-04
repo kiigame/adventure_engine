20352384ab9ff162e26ed1dbd7bbd178013d5570
Eino Keskitalo <eino.keskitalo@gmail.com>
2025-10-04 17:56:05 +0300

    refactor: rename a const

--- src/kiigame.js
+++ src/kiigame.js
@@ -229,17 +229,17 @@ export class KiiGame {
         });
 
         // Load up frames from json to the character animations array.
-        const animation_data = gameData.character_json.animations;
-        for (const i in animation_data) {
+        const characterAnimationData = gameData.character_json.animations;
+        for (const i in characterAnimationData) {
             const frames = [];
-            for (const j in animation_data[i].frames) {
+            for (const j in characterAnimationData[i].frames) {
                 const frame = new Konva.Tween({
-                    node: this.getObject(animation_data[i].frames[j].node),
-                    duration: animation_data[i].frames[j].duration
+                    node: this.getObject(characterAnimationData[i].frames[j].node),
+                    duration: characterAnimationData[i].frames[j].duration
                 });
                 frames.push(frame);
             }
-            this.character_animations[animation_data[i].id] = frames;
+            this.character_animations[characterAnimationData[i].id] = frames;
         }
 
         // Set up onFinish functions for each frame to show the next frame. In the case
