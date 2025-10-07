6ae1d94075acdae1a293584c32b00bf7d616f030
Eino Keskitalo <eino.keskitalo@gmail.com>
2025-10-07 21:45:05 +0300

    test(#232): stop waiting for setInterval to finish in the music test

--- src/view/Music.test.js
+++ src/view/Music.test.js
@@ -5,9 +5,9 @@ import AudioFactory from './music/AudioFactory.js';
 
 const audioFactoryStub = sinon.createStubInstance(AudioFactory);
 
-describe('Test Music', function() {
-    it('playing undefined music does not crash', function() {
-        function AudioStub() {};
+describe('Test Music', function () {
+    it('playing undefined music does not crash', function () {
+        function AudioStub() { };
         audioFactoryStub.create.returns(new AudioStub());
         const json = {
             "layer": {
@@ -19,7 +19,7 @@ describe('Test Music', function() {
         const result = music.current_music;
         assert.deepEqual(result, null);
     });
-    it('starting music without loop, fade_in or fade_out data will not have loop, fade_in or fade_out', function() {
+    it('starting music without loop, fade_in or fade_out data will not have loop, fade_in or fade_out', function () {
         function AudioStub() {
             this.play = function () {
                 // do nothing
@@ -40,7 +40,7 @@ describe('Test Music', function() {
         assert.isFalse(result.fade_out);
         assert.deepEqual(result.volume, 1);
     });
-    it('starting music with explicit false for loop, fade_in and fade_out in data will not fave loop, fade_in or fade_out', function() {
+    it('starting music with explicit false for loop, fade_in and fade_out in data will not fave loop, fade_in or fade_out', function () {
         function AudioStub() {
             this.play = function () {
                 // do nothing
@@ -64,32 +64,7 @@ describe('Test Music', function() {
         assert.isFalse(result.fade_out);
         assert.deepEqual(result.volume, 1);
     });
-    it('starting music with loop, fade_in and fade_out in data have all of loop, fade_in and fade_out', function() {
-        function AudioStub() {
-            this.play = function () {
-                // do nothing
-            }
-        };
-        audioFactoryStub.create.returns(new AudioStub());
-        const json = {
-            "layer": {
-                "music": "music.ogg",
-                "fade_in": true,
-                "fade_out": true,
-                "loop": true,
-            },
-        };
-        const music = new Music(json, audioFactoryStub);
-        music.playMusicById('layer');
-        const result = music.current_music;
-        assert.isNotNull(result);
-        assert.isTrue(result.loop);
-        assert.isTrue(result.fade_in);
-        assert.isTrue(result.fade_out);
-        // Cludgy way to test that the volume starts at 0
-        assert.deepEqual(result.volume, 0);
-    });
-    it('in two subsequent rooms with same music, respect if the second room implicitly sets looping to false', function() {
+    it('in two subsequent rooms with same music, respect if the second room implicitly sets looping to false', function () {
         function AudioStub() {
             this.play = function () {
                 // do nothing
@@ -111,7 +86,7 @@ describe('Test Music', function() {
         const result = music.current_music;
         assert.isFalse(result.loop);
     });
-    it('in two subsequent rooms with same music, respect if the second room explicitly sets looping to false', function() {
+    it('in two subsequent rooms with same music, respect if the second room explicitly sets looping to false', function () {
         function AudioStub() {
             this.play = function () {
                 // do nothing
@@ -132,6 +107,41 @@ describe('Test Music', function() {
         music.playMusicById('noloop');
         const result = music.current_music;
         assert.isFalse(result.loop);
+    });
+    describe('fades', function () {
+        let clock;
+        before(function () {
+            clock = sinon.useFakeTimers();
+        });
+        after(function () {
+            clock.tick(100000); // so that mocha doesn't wait for the interval to resolve
+            clock.restore();
+        });
+        it('starting music with loop, fade_in and fade_out in data have all of loop, fade_in and fade_out', function () {
+            function AudioStub() {
+                this.play = function () {
+                    // do nothing
+                }
+            };
+            audioFactoryStub.create.returns(new AudioStub());
+            const json = {
+                "layer": {
+                    "music": "music.ogg",
+                    "fade_in": true,
+                    "fade_out": true,
+                    "loop": true,
+                },
+            };
+            const music = new Music(json, audioFactoryStub);
+            music.playMusicById('layer');
+            const result = music.current_music;
+            assert.isNotNull(result);
+            assert.isTrue(result.loop);
+            assert.isTrue(result.fade_in);
+            assert.isTrue(result.fade_out);
+            // Test that volume starts at zero when fading in
+            assert.deepEqual(result.volume, 0);
+        });
     });
     // TODO: More test cases
 });
