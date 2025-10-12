6981c484f997c88d33432d4829c6f3b3bc0fc501
Eino Keskitalo <eino.keskitalo@gmail.com>
2025-10-12 10:23:22 +0300

    test: test doInstantTransition

--- src/model/CharacterInRoom.test.js
+++ src/model/CharacterInRoom.test.js
@@ -3,7 +3,7 @@ import { createStubInstance } from 'sinon';
 import CharacterInRoom from './CharacterInRoom.js';
 import EventEmitter from '../events/EventEmitter.js';
 
-describe('Character in room model tests', function () {
+describe('Character in room model tests', () => {
     let uiEventEmitterStub;
     let gameEventEmitterStub;
     beforeEach(() => {
@@ -13,8 +13,8 @@ describe('Character in room model tests', function () {
             emit: () => null
         });
     });
-    describe('start transition to room', function () {
-        it('should go from NOT_IN_ROOM to IN_ROOM directly', function () {
+    describe('start transition to room', () => {
+        it('should go from NOT_IN_ROOM to IN_ROOM directly', () => {
             const characterInRoom = new CharacterInRoom(uiEventEmitterStub, gameEventEmitterStub);
             const startTransitionToRoomCallback = gameEventEmitterStub.on.getCalls().find((callback) => {
                 return callback.args[0] === 'do_transition';
@@ -33,7 +33,7 @@ describe('Character in room model tests', function () {
                 'arrived_in_room not emitted with room_id'
             ).to.equal(true);
         });
-        it('should go from IN_ROOM to IN_TRANSITION', function () {
+        it('should go from IN_ROOM to IN_TRANSITION', () => {
             const characterInRoom = new CharacterInRoom(uiEventEmitterStub, gameEventEmitterStub);
             characterInRoom.state = {
                 mode: 'IN_ROOM',
@@ -52,7 +52,7 @@ describe('Character in room model tests', function () {
                 'leaving_room not emitted'
             ).to.equal(true);
         });
-        it('should throw if transtioning while in transition', function () {
+        it('should throw if transtioning while in transition', () => {
             const characterInRoom = new CharacterInRoom(uiEventEmitterStub, gameEventEmitterStub);
             characterInRoom.state = {
                 mode: 'IN_TRANSITION',
@@ -65,6 +65,46 @@ describe('Character in room model tests', function () {
             expect(() =>
                 startTransitionToRoomCallback({ roomId: 'room-id' })
             ).to.throw('Doing transition with incompatible CharacterInRoom mode IN_TRANSITION');
+        });
+    });
+    describe('instant transition to room', () => {
+        it('should go from NOT_IN_ROOM to IN_ROOM directly', () => {
+            const characterInRoom = new CharacterInRoom(uiEventEmitterStub, gameEventEmitterStub);
+            const doInstantTransitionCallback = gameEventEmitterStub.on.getCalls().find((callback) => {
+                return callback.args[0] === 'do_instant_transition';
+            }).args[1];
+            doInstantTransitionCallback({ roomId: 'room-id' });
+            expect(
+                characterInRoom.state,
+                'state does not match'
+            ).to.deep.equal({ mode: 'IN_ROOM', room: 'room-id' });
+            expect(
+                gameEventEmitterStub.emit.calledWith('arrived_in_room', 'room-id'),
+                'arrived_in_room not emitted with room-id'
+            ).to.equal(true);
+        });
+        it('should go from IN_ROOM to IN_ROOM', function () {
+            const characterInRoom = new CharacterInRoom(uiEventEmitterStub, gameEventEmitterStub);
+            characterInRoom.state = {
+                mode: 'IN_ROOM',
+                room: 'another-room'
+            };
+            const doInstantTransitionCallback = gameEventEmitterStub.on.getCalls().find((callback) => {
+                return callback.args[0] === 'do_instant_transition';
+            }).args[1];
+            doInstantTransitionCallback({ roomId: 'room-id' });
+            expect(
+                characterInRoom.state,
+                'state does not match'
+            ).to.deep.equal({ mode: 'IN_ROOM', room: 'room-id' });
+            expect(
+                gameEventEmitterStub.emit.calledWith('left_room', 'another-room'),
+                'left_room not emitted with another-room'
+            ).to.equal(true);
+            expect(
+                gameEventEmitterStub.emit.calledWith('arrived_in_room', 'room-id'),
+                'arrived_in_room not emitted with room-id'
+            ).to.equal(true);
         });
     });
 });
