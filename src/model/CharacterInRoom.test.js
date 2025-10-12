1da1cf3aba627982c00efe35b69e9114c628a9bb
Eino Keskitalo <eino.keskitalo@gmail.com>
2025-10-12 09:57:29 +0300

    test: expect event emitting (and stry out expect from chai)

--- src/model/CharacterInRoom.test.js
+++ src/model/CharacterInRoom.test.js
@@ -1,5 +1,5 @@
-import { assert } from 'chai';
-import { createStubInstance, stub } from 'sinon';
+import { expect } from 'chai';
+import { createStubInstance } from 'sinon';
 import CharacterInRoom from './CharacterInRoom.js';
 import EventEmitter from '../events/EventEmitter.js';
 
@@ -8,7 +8,10 @@ describe('Character in room model tests', function () {
     let gameEventEmitterStub;
     beforeEach(() => {
         uiEventEmitterStub = createStubInstance(EventEmitter, { on: () => null });
-        gameEventEmitterStub = createStubInstance(EventEmitter, { on: () => null });
+        gameEventEmitterStub = createStubInstance(EventEmitter, {
+            on: () => null,
+            emit: () => null
+        });
     });
     describe('start transition to room', function () {
         it('should go from NOT_IN_ROOM to IN_ROOM directly', function () {
@@ -17,7 +20,18 @@ describe('Character in room model tests', function () {
                 return callback.args[0] === 'do_transition';
             }).args[1];
             startTransitionToRoomCallback({ roomId: 'room-id' });
-            assert.deepEqual(characterInRoom.state, { mode: 'IN_ROOM', room: 'room-id' });
+            expect(
+                characterInRoom.state,
+                'state does not match'
+            ).to.deep.equal({ mode: 'IN_ROOM', room: 'room-id' });
+            expect(
+                gameEventEmitterStub.emit.calledWith('arriving_in_room'),
+                'arriving_in_room not emitted'
+            ).to.equal(true);
+            expect(
+                gameEventEmitterStub.emit.calledWith('arrived_in_room', 'room-id'),
+                'arrived_in_room not emitted with room_id'
+            ).to.equal(true);
         });
     });
 });
