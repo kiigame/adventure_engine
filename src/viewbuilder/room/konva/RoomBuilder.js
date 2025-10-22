8fb7332292a6d542403f3fd1e095b841b55e2184
Eino Keskitalo <eino.keskitalo@gmail.com>
2025-10-22 15:31:03 +0300

    refactor(#125): build room background images from simplified non-Konva specific data

--- src/viewbuilder/room/konva/RoomBuilder.js
+++ src/viewbuilder/room/konva/RoomBuilder.js
@@ -5,6 +5,42 @@ class RoomBuilder {
      */
     build(roomJson) {
         roomJson.attrs.visible = false;
+        roomJson = this.buildBackground(roomJson);
+        return roomJson;
+    }
+
+    buildBackground(roomJson) {
+        // TODO: make configurable / responsive / etc / etc
+        const bgWidth = 981;
+        const bgHeight = 543;
+
+        if (Object.keys(roomJson).includes('background')) {
+            const backgroundJson = JSON.parse(JSON.stringify(roomJson.background));
+            delete roomJson.background;
+            if (!backgroundJson || !Object.keys(backgroundJson).length) {
+                return roomJson;
+            }
+            if (!roomJson.children) {
+                roomJson.children = [];
+            }
+            const background = {};
+            if (backgroundJson.src) {
+                background.attrs = {
+                    "category": "room_background",
+                    "src": backgroundJson.src,
+                    "visible": true,
+                    "width": bgWidth,
+                    "height": bgHeight
+                };
+                background.className = "Image";
+            }
+            if (backgroundJson.id) {
+                background.attrs.id = backgroundJson.id;
+            }
+            if (Object.keys(background).length > 0) {
+                roomJson.children.unshift(background);
+            }
+        }
         return roomJson;
     }
 }
--- src/viewbuilder/room/konva/RoomBuilder.test.js
+++ src/viewbuilder/room/konva/RoomBuilder.test.js
@@ -1,22 +1,161 @@
 import { expect } from 'chai';
 import RoomBuilder from './RoomBuilder.js';
 
-describe('konva room builder tests', function () {
-    it('test visibility is set to false the room', function () {
-        const roomBuilder = new RoomBuilder();
-        const roomJson = {
-        "attrs": {
-            "category": "room",
-            "id": "roomy_room"
-        }};
-        const expected = {
-            "attrs": {
-                "category": "room",
-                "id": "roomy_room",
-                "visible": false
-            }
-        };
-        const result = roomBuilder.build(roomJson);
-        expect(result).to.deep.equal(expected);
+describe('konva room builder tests', () => {
+    describe('visibility', () => {
+        it('should set initial visibility to false', () => {
+            const roomBuilder = new RoomBuilder();
+            const roomJson = {
+                "attrs": {
+                    "category": "room",
+                    "id": "roomy_room"
+                }
+            };
+            const expected = {
+                "attrs": {
+                    "category": "room",
+                    "id": "roomy_room",
+                    "visible": false
+                }
+            };
+            const result = roomBuilder.build(roomJson);
+            expect(result).to.deep.equal(expected);
+        });
+    });
+    describe('background image', () => {
+        it('should set background image dimensions to the default room view size', () => {
+            const roomBuilder = new RoomBuilder();
+            const roomJson = {
+                "attrs": {
+                    "category": "room",
+                    "id": "roomy_room"
+                },
+                "background": {
+                    "id": "roomy_room_bg",
+                    "src": "data/images/locker_room_1.png"
+                }
+            };
+            const expected = {
+                "attrs": {
+                    "category": "room",
+                    "id": "roomy_room",
+                    "visible": false
+                },
+                "children": [
+                    {
+                        "attrs": {
+                            "category": "room_background",
+                            "id": "roomy_room_bg",
+                            "src": "data/images/locker_room_1.png",
+                            "visible": true,
+                            "width": 981,
+                            "height": 543
+                        },
+                        "className": "Image"
+                    }
+                ]
+            };
+            const result = roomBuilder.build(roomJson);
+            expect(result).to.deep.equal(expected);
+        });
+        it('should insert background as the first child of the room so that it will not cover furniture', () => {
+            const roomBuilder = new RoomBuilder();
+            const roomJson = {
+                "attrs": {
+                    "category": "room",
+                    "id": "roomy_room"
+                },
+                "background": {
+                    "id": "roomy_room_bg",
+                    "src": "data/images/locker_room_1.png"
+                },
+                "children": [
+                    {
+                        "data": "mock_data"
+                    }
+                ]
+            };
+            const expected = {
+                "attrs": {
+                    "category": "room",
+                    "id": "roomy_room",
+                    "visible": false
+                },
+                "children": [
+                    {
+                        "attrs": {
+                            "category": "room_background",
+                            "id": "roomy_room_bg",
+                            "src": "data/images/locker_room_1.png",
+                            "visible": true,
+                            "width": 981,
+                            "height": 543
+                        },
+                        "className": "Image"
+                    },
+                    {
+                        "data": "mock_data"
+                    }
+                ]
+            };
+            const result = roomBuilder.build(roomJson);
+            expect(result).to.deep.equal(expected);
+        });
+        it('should handle rooms without background object in data gracefully', () => {
+            const roomBuilder = new RoomBuilder();
+            const roomJson = {
+                "attrs": {
+                    "category": "room",
+                    "id": "roomy_room"
+                }
+            };
+            const expected = {
+                "attrs": {
+                    "category": "room",
+                    "id": "roomy_room",
+                    "visible": false
+                }
+            };
+            const result = roomBuilder.build(roomJson);
+            expect(result).to.deep.equal(expected);
+        });
+        it('should handle rooms with null background object in data gracefully', () => {
+            const roomBuilder = new RoomBuilder();
+            const roomJson = {
+                "attrs": {
+                    "category": "room",
+                    "id": "roomy_room"
+                },
+                "background": null
+            };
+            const expected = {
+                "attrs": {
+                    "category": "room",
+                    "id": "roomy_room",
+                    "visible": false
+                }
+            };
+            const result = roomBuilder.build(roomJson);
+            expect(result).to.deep.equal(expected);
+        });
+        it('should handle rooms with empty background object in data gracefully', () => {
+            const roomBuilder = new RoomBuilder();
+            const roomJson = {
+                "attrs": {
+                    "category": "room",
+                    "id": "roomy_room"
+                },
+                "background": {}
+            };
+            const expected = {
+                "attrs": {
+                    "category": "room",
+                    "id": "roomy_room",
+                    "visible": false
+                }
+            };
+            const result = roomBuilder.build(roomJson);
+            expect(result).to.deep.equal(expected);
+        });
     });
 });
