24d5977934f09501fbbdd00297f3daf7da123a03
Eino Keskitalo <eino.keskitalo@gmail.com>
2025-10-24 11:53:42 +0300

    test(#251): add test cases for removing or adding multiple objects of different types at once

--- src/model/ObjectsInRooms.test.js
+++ src/model/ObjectsInRooms.test.js
@@ -59,6 +59,31 @@ describe('Objects in rooms model tests', () => {
                 }
             );
         });
+        it('should set visible to false for visible objects of different types existing in room', () => {
+            new ObjectsInRooms(initialState, gameEventEmitterStub, uiEventEmitterStub);
+            const removeObjectsCallback = gameEventEmitterStub.on.getCalls().find((callback) => {
+                return callback.args[0] === 'remove_objects';
+            }).args[1];
+            removeObjectsCallback(['object_1', 'object_4']);
+            expect(uiEventEmitterStub.emit, 'removed_objects not emitted as expected').to.have.been.calledWith(
+                'removed_objects',
+                {
+                    'objectList': {
+                        'room_one': {
+                            'furniture': {
+                                'object_1': { 'visible': false },
+                                'object_2': { 'visible': false }
+                            },
+                            'other_type': {
+                                'object_3': { 'visible': false },
+                                'object_4': { 'visible': false }
+                            }
+                        },
+                    },
+                    'objectsRemoved': ['object_1', 'object_4']
+                }
+            );
+        });
     });
     describe('add objects to room', () => {
         it('should set visible to true for a non-visible object existing in room', () => {
@@ -83,6 +108,31 @@ describe('Objects in rooms model tests', () => {
                         },
                     },
                     'objectsAdded': ['object_2']
+                }
+            );
+        });
+                it('should set visible to true for non-visible objects of different types existing in room', () => {
+            new ObjectsInRooms(initialState, gameEventEmitterStub, uiEventEmitterStub);
+            const addObjectsCallback = gameEventEmitterStub.on.getCalls().find((callback) => {
+                return callback.args[0] === 'add_objects';
+            }).args[1];
+            addObjectsCallback(['object_2', 'object_3']);
+            expect(uiEventEmitterStub.emit, 'added_objects not emitted as expected').to.have.been.calledWith(
+                'added_objects',
+                {
+                    'objectList': {
+                        'room_one': {
+                            'furniture': {
+                                'object_1': { 'visible': true },
+                                'object_2': { 'visible': true }
+                            },
+                            'other_type': {
+                                'object_3': { 'visible': true },
+                                'object_4': { 'visible': true }
+                            }
+                        },
+                    },
+                    'objectsAdded': ['object_2', 'object_3']
                 }
             );
         });
