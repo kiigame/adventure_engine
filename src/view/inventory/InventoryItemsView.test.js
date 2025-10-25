c384cc21aacc9e7717c572c39dba125ae978178c
Eino Keskitalo <eino.keskitalo@gmail.com>
2025-10-25 15:47:11 +0300

    test(#248): refactor test for stub reusability and (I hope) clarity

--- src/view/inventory/InventoryItemsView.test.js
+++ src/view/inventory/InventoryItemsView.test.js
@@ -10,13 +10,22 @@ use(sinonChai);
 describe('inventory item view tests', () => {
     let gameEventEmitterStub;
     let uiEventEmitterStub;
-    let inventoryItemsStub;
+    let inventoryItemsGroupStub;
     let inventoryItemsCollectionStub;
+    let inventoryItemStub;
+    let inventoryItemStubs;
     beforeEach(() => {
         uiEventEmitterStub = createStubInstance(EventEmitter);
         gameEventEmitterStub = createStubInstance(EventEmitter);
-        inventoryItemsCollectionStub = createStubInstance(Collection, { each: null });
-        inventoryItemsStub = createStubInstance(Group, { getChildren: inventoryItemsCollectionStub });
+        inventoryItemStub = createStubInstance(Shape);
+        inventoryItemStubs = [inventoryItemStub];
+        inventoryItemsCollectionStub = createStubInstance(Collection);
+        inventoryItemsCollectionStub.each.yields(inventoryItemStub);
+        inventoryItemsGroupStub = createStubInstance(Group, {
+            getChildren: inventoryItemsCollectionStub,
+            find: inventoryItemStub
+        });
+        inventoryItemsGroupStub.findOne.yields(inventoryItemStub).returns(inventoryItemStub);
     });
     afterEach(() => {
         restore();
@@ -24,116 +33,117 @@ describe('inventory item view tests', () => {
     describe('reset inventory items', () => {
         // Don't know how to stub multiple shapes in the collection!
         it('should set Shapes in the inventory items group to not visible', () => {
-            const shapeStub = createStubInstance(Shape);
-            inventoryItemsCollectionStub.each.yields(shapeStub);
-            inventoryItemsStub.find.returns(shapeStub);
             const inventoryItemsView = new InventoryItemsView(
                 uiEventEmitterStub,
                 gameEventEmitterStub,
-                inventoryItemsStub,
+                inventoryItemsGroupStub,
                 100
             );
             inventoryItemsView.resetItems();
-            expect(shapeStub.setAttr).to.have.been.calledWith('visible', false);
+            expect(inventoryItemStub.setAttr).to.have.been.calledWith('visible', false);
         });
     });
     describe('handle inventory item visibilty', () => {
         it('should set existing item in the inventory visible', () => {
-            const shapeStub = createStubInstance(Shape);
-            shapeStub.attrs = { id: 'blabla' };
-            inventoryItemsStub.find.returns(shapeStub);
-            inventoryItemsStub.findOne.yields(shapeStub).returns(shapeStub);
+            inventoryItemStub.attrs = { id: 'blabla' };
             const inventoryItemsView = new InventoryItemsView(
                 uiEventEmitterStub,
                 gameEventEmitterStub,
-                inventoryItemsStub,
+                inventoryItemsGroupStub,
                 100
             );
             inventoryItemsView.handleInventoryItemVisibility(['blabla']);
-            expect(shapeStub.x).to.have.been.calledWith(50);
-            expect(shapeStub.y).to.have.been.calledWith(100);
-            expect(shapeStub.setAttr).to.have.been.calledWith('visible', true);
+            expect(inventoryItemStub.x).to.have.been.calledWith(50);
+            expect(inventoryItemStub.y).to.have.been.calledWith(100);
+            expect(inventoryItemStub.setAttr).to.have.been.calledWith('visible', true);
         });
         it('should set multiple inventory items\' y coordinates correctly', () => {
-            const shapeStubs = [
+            inventoryItemStubs = [
                 createStubInstance(Shape),
                 createStubInstance(Shape),
                 createStubInstance(Shape),
             ];
-
-            shapeStubs.forEach((shapeStub, index) => {
-                inventoryItemsStub.findOne.onCall(index).returns(shapeStub);
+            inventoryItemStubs.forEach((shapeStub, index) => {
+                inventoryItemsGroupStub.findOne.onCall(index).returns(shapeStub);
             });
-            inventoryItemsStub.find.returns(shapeStubs[0]);
+            inventoryItemsGroupStub.find.returns(inventoryItemStubs[0]);
+
             const inventoryItemsView = new InventoryItemsView(
                 uiEventEmitterStub,
                 gameEventEmitterStub,
-                inventoryItemsStub,
+                inventoryItemsGroupStub,
                 100
             );
             inventoryItemsView.handleInventoryItemVisibility(['1', '2', '3']);
-            expect(shapeStubs[0].x).to.have.been.calledWith(50);
-            expect(shapeStubs[1].x).to.have.been.calledWith(150);
-            expect(shapeStubs[2].x).to.have.been.calledWith(250);
+            expect(inventoryItemStubs[0].x).to.have.been.calledWith(50);
+            expect(inventoryItemStubs[1].x).to.have.been.calledWith(150);
+            expect(inventoryItemStubs[2].x).to.have.been.calledWith(250);
         });
     });
     describe('clear inventory item blur', () => {
         // Don't know how to stub multiple shapes in the collection!
         it('should clear blur from items in inventory', () => {
-            const shapeStub = createStubInstance(Shape);
-            inventoryItemsCollectionStub.each.yields(shapeStub);
-            inventoryItemsStub.find.returns(shapeStub);
             const inventoryItemsView = new InventoryItemsView(
                 uiEventEmitterStub,
                 gameEventEmitterStub,
-                inventoryItemsStub,
+                inventoryItemsGroupStub,
                 100
             );
             inventoryItemsView.clearInventoryItemBlur();
-            expect(shapeStub.shadowBlur).to.have.been.calledWith(0);
+            expect(inventoryItemStub.shadowBlur).to.have.been.calledWith(0);
         });
     });
     describe('glow inventory item', () => {
         it('should glow an item if found', () => {
-            const shapeStub = createStubInstance(Shape);
-            inventoryItemsStub.findOne.yields(shapeStub);
-            inventoryItemsStub.find.returns(shapeStub);
             const inventoryItemsView = new InventoryItemsView(
                 uiEventEmitterStub,
                 gameEventEmitterStub,
-                inventoryItemsStub,
+                inventoryItemsGroupStub,
                 100
             );
-            inventoryItemsView.glowInventoryItem(shapeStub);
-            expect(shapeStub.clearCache).to.have.been.called;
-            expect(shapeStub.shadowColor).to.have.been.calledWith('purple');
-            expect(shapeStub.shadowOffset).to.have.been.calledWith({ x: 0, y: 0});
-            expect(shapeStub.shadowBlur).to.have.been.calledWith(20);
+            inventoryItemsView.glowInventoryItem(inventoryItemStub);
+            expect(inventoryItemStub.clearCache).to.have.been.called;
+            expect(inventoryItemStub.shadowColor).to.have.been.calledWith('purple');
+            expect(inventoryItemStub.shadowOffset).to.have.been.calledWith({ x: 0, y: 0});
+            expect(inventoryItemStub.shadowBlur).to.have.been.calledWith(20);
         });
         it('should not glow an item not found', () => {
-            const shapeStub = createStubInstance(Shape);
-            inventoryItemsStub.findOne.yields(null);
-            inventoryItemsStub.find.returns(shapeStub);
+            inventoryItemsGroupStub.findOne.yields(null).returns(null);
             const inventoryItemsView = new InventoryItemsView(
                 uiEventEmitterStub,
                 gameEventEmitterStub,
-                inventoryItemsStub,
+                inventoryItemsGroupStub,
                 100
             );
-            inventoryItemsView.glowInventoryItem(shapeStub);
-            expect(shapeStub.clearCache).to.not.have.been.called;
-            expect(shapeStub.shadowColor).to.not.have.been.called;
-            expect(shapeStub.shadowOffset).to.not.have.been.called;
-            expect(shapeStub.shadowBlur).to.not.have.been.called;
+            inventoryItemsView.glowInventoryItem(inventoryItemStub);
+            expect(inventoryItemStub.clearCache).to.not.have.been.called;
+            expect(inventoryItemStub.shadowColor).to.not.have.been.called;
+            expect(inventoryItemStub.shadowOffset).to.not.have.been.called;
+            expect(inventoryItemStub.shadowBlur).to.not.have.been.called;
+        });
+    });
+    describe('fire inventory item drag end event on Konva dragend', () => {
+        it('should fire inventory_item_drag_end event on dragend of an inventory item', () => {
+            new InventoryItemsView(
+                uiEventEmitterStub,
+                gameEventEmitterStub,
+                inventoryItemsGroupStub,
+                100
+            );
+            const dragendHandler = inventoryItemStub.on.getCalls().find((call) => {
+                return call.args[0] === 'dragend';
+            }).args[1];
+            const eventStub = { target: inventoryItemStub };
+            dragendHandler(eventStub);
+            expect(uiEventEmitterStub.emit).to.have.been.calledWith('inventory_item_drag_end', { draggedItem: inventoryItemStub });
         });
     });
     describe('move dragged item back to inventory', () => {
         it('should move the dragged item back to inventory after dragging', () => {
-            inventoryItemsStub.find.returns(createStubInstance(Shape));
             new InventoryItemsView(
                 uiEventEmitterStub,
                 gameEventEmitterStub,
-                inventoryItemsStub,
+                inventoryItemsGroupStub,
                 100
             );
             const moveDraggedItemBackToInventoryCallback = uiEventEmitterStub.on.getCalls().find((callback) => {
@@ -141,14 +151,13 @@ describe('inventory item view tests', () => {
             }).args[1];
             const targetStub = createStubInstance(Shape);
             moveDraggedItemBackToInventoryCallback(targetStub);
-            expect(targetStub.moveTo).to.have.been.calledWith(inventoryItemsStub);
+            expect(targetStub.moveTo).to.have.been.calledWith(inventoryItemsGroupStub);
         });
         it('should not try to move a non-existing dragged item back to inventory after dragging (null safety)', () => {
-            inventoryItemsStub.find.returns(createStubInstance(Shape));
             new InventoryItemsView(
                 uiEventEmitterStub,
                 gameEventEmitterStub,
-                inventoryItemsStub,
+                inventoryItemsGroupStub,
                 100
             );
             const moveDraggedItemBackToInventoryCallback = uiEventEmitterStub.on.getCalls().find((callback) => {
